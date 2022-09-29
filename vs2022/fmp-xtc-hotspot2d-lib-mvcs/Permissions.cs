89aa58dc9d4ffb5f2f313a05a3823aa24ac2c675
Easlee Liu <easlee@outlook.com>
2022-09-30 00:54:10 +0800

    regenerated

---
+++ vs2022/fmp-xtc-hotspot2d-lib-mvcs/Permissions.cs
@@ -0,0 +1,18 @@
+
+namespace XTC.FMP.MOD.Hotspot2D.LIB.MVCS
+{
+    public class Permissions
+    {
+
+        public const string DesignerCreate = "/XTC/Hotspot2D/Designer/Create";
+        public const string DesignerUpdate = "/XTC/Hotspot2D/Designer/Update";
+        public const string DesignerRetrieve = "/XTC/Hotspot2D/Designer/Retrieve";
+        public const string DesignerDelete = "/XTC/Hotspot2D/Designer/Delete";
+
+        public const string HealthyCreate = "/XTC/Hotspot2D/Healthy/Create";
+        public const string HealthyUpdate = "/XTC/Hotspot2D/Healthy/Update";
+        public const string HealthyRetrieve = "/XTC/Hotspot2D/Healthy/Retrieve";
+        public const string HealthyDelete = "/XTC/Hotspot2D/Healthy/Delete";
+
+    }
+}
